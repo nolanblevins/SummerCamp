828ebdbf1e9b92aa5b6bb98f7f831004ebb8eb03
Nolan Blevins <npblevins@gmail.com>
2022-10-17 12:01:11 -0400

    scrum

--- Child.java
+++ Child.java
@@ -1,7 +1,9 @@
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.UUID;
 
 public class Child {
+    protected UUID id;
     protected String firstName;
     protected String lastName;
     protected ArrayList<String> notes;
@@ -9,12 +11,19 @@ public class Child {
     protected Date birthday;
     
     public Child(String firstName, String lastName, MedicalInfo medicalInfo, Date birthday) {
+        this.id = UUID.randomUUID();
         this.firstName = firstName;
         this.lastName = lastName;
         this.medicalInfo = medicalInfo;
         this.birthday = birthday;
     }
 
-    
+    public Child(UUID id, String firstName, String lastName, MedicalInfo medicalInfo, Date birthday) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.medicalInfo = medicalInfo;
+        this.birthday = birthday;
+    }
     
 }
