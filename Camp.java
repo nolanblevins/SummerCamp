e2f42214b61f4884fd4b8dc92f8988f179e2d501
unknown <jonahandrews7@gmail.com>
2022-10-16 22:56:21 -0400

    Added methods to Camp class

--- Camp.java
+++ Camp.java
@@ -11,6 +11,24 @@ public class Camp {
         this.time = time;
         this.price = price;
     }
+
+    /**
+     * Returns the camp's ArrayList of type Group
+     * 
+     * @return ArrayList<Group> holds all the groups that are in the camp
+     */
+    public ArrayList<Group> getGroups() {
+        return this.groups;
+    }
+
+    /**
+     * Returns a string that summarizes the important attributes of the camp
+     * 
+     * @return String   is a breif description of the camp's data
+     */
+    public String toString() {
+        return ("Time: "+ this.time+" Price: "+this.price);
+    }
     
     
     
--- User.java
+++ User.java
@@ -13,7 +13,6 @@ public class User {
         this.password = password;
     }
 
-
     
     
 }
