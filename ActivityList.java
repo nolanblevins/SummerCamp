f537db39b2d766d614e9aa63b993ebe381f83bc8
gward23 <grantward23@outlook.com>
2022-10-18 09:41:54 -0400

    pushing ActivityList

--- ActivityList.java
+++ ActivityList.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class ActivityList {
+import java.util.ArrayList;
+
+public class ActivityList {
+    private ArrayList<Activity> activites;
+    private ActivityList activityList;
+
+    private ActivityList ActivityList() {
+        return null;
+    }
+
+    public ActivityList getInstance() {
+        return null;
+    }
+
+    public void addActivity(Activity activity) {
+
+    }
+
+    public ArrayList<Activity> getAllActivities() {
+        return null;
+    }
+
+    public void editActivity(Activity activity) {
+
+    }
+
+    public void saveActivity() {
+
+    }
 }
