e97baa552d8e09b1d233dddef8ca57560a8dd8f8
mh1300 <89536791+mh1300@users.noreply.github.com>
2022-10-18 09:42:12 -0400

    GroupList stub

--- GroupList.java
+++ GroupList.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class GroupList {
+import java.util.ArrayList;
+public class GroupList {
+    private ArrayList<Group> groups;
+    private static GroupList groupList;
+
+    private GroupList(){
+        this.groups = DataReader.getAllGroups();
+    }
+
+    public static GroupList getInstance(){
+        if(groupList == null){
+            groupList = new GroupList();
+        }
+        return groupList;
+    }
+
+    public void addGroup(){
+
+    }
+
+    public Group getGroup(){
+        return null;
+    }
+
+    public void editGroup(Group group){
+
+    }
+
+    public void saveGroups(){
+
+    }
 }
+
