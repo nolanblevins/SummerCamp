deebb6ba98a3dec85418e63bd21e80c706b28309
mh1300 <89536791+mh1300@users.noreply.github.com>
2022-10-18 08:50:28 -0400

    Some Data Stubs

--- DataConstants.java
+++ DataConstants.java
@@ -1,2 +1,57 @@
-package JSON;public class DataConstants {
+
+public class DataConstants {
+    DataConstants dataConstants;
+    String[] userData;
+    String[] rUData;
+    String[] counselorData;
+    String[] campData;
+    String[] groupData;
+    String[] childData;
+    String[] activityData;
+
+    private DataConstants(){
+        this.setUserData();
+        this.setrUData();
+        this.setCounselorData();
+        this.setCampData();
+        this.setGroupData();
+        this.setChildData();
+        this.setActivityData();
+    }
+
+    private DataConstants getInstance(){
+        if(this.dataConstants == null){
+            this.dataConstants = new DataConstants();
+        }
+        return this.dataConstants;
+    }
+
+    private void setUserData(){
+
+    }
+
+    private void setrUData(){
+
+    }
+
+    private void setCounselorData(){
+
+    }
+
+    private void setCampData(){
+
+    }
+
+    private void setGroupData(){
+
+    }
+
+    private void setChildData(){
+
+    }
+
+    private void setActivityData(){
+
+    }
+
 }
--- DataReader.java
+++ DataReader.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class DataReader {
+import java.util.ArrayList;
+
+public class DataReader {
+    ArrayList<User> userList;
+    ArrayList<Camp> campList;
+    ArrayList<Group> groupList;
+    ArrayList<Child> childList;
+    ArrayList<Activity> activityList;
+
+    DataConstants dataConstants;
+
+    public static ArrayList<User> getAllUsers(){
+        return null;
+    }
+
+    public static ArrayList<Camp> getAllCamps(){
+        return null;
+    }
+
+    public static ArrayList<Group> getAllGroups(){
+        return null;
+    }
+
+    public static ArrayList<Activity> getAllActivities(){
+        return null;
+    }
 }
