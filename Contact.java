a0f57e18b1715a64445a8eebb665412ddd8ae905
unknown <jonahandrews7@gmail.com>
2022-10-18 09:46:59 -0400

    contact updated

--- Contact.java
+++ Contact.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Contact {
+public class Contact {
+
+private String name;
+private String phoneNumber;
+private String relationship;
+
+public Contact(String name, String phoneNumber, String relationship) {
+    this.name = name;
+    this.phoneNumber = phoneNumber;
+    this.relationship = relationship;
+}
+
+public getContact() {
+    return this;
+}
+
+
 }
