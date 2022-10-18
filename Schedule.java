99b4e08a08d545cb252f0f80a5d7ad2016b96081
gward23 <grantward23@outlook.com>
2022-10-18 09:25:53 -0400

    pushing Schedule

--- Schedule.java
+++ Schedule.java
@@ -3,5 +3,16 @@ import java.util.ArrayList;
 public class Schedule {
     private ArrayList<Activity> schedule;
     
+    public Schedule getSchedule(WeekDay weekday) {
+        return null;
+    }
+
+    public ArrayList<Activity> generateSchedule() {
+        return null;
+    }
+
+    public String toString() {
+        return null;
+    }
     
 }
