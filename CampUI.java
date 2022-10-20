b62925dc5326d059f4f7edde98c67dac6224fcdf
unknown <jonahandrews7@gmail.com>
2022-10-20 09:45:19 -0400

    updated UI with menuSelection

--- CampUI.java
+++ CampUI.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
+
 public class CampUI {
 
-    public static void main(String[] args) {
+    private static final int menuLength = 5;
+
+	public static void main(String[] args) {
 
         headerIntro();
-    
+        
+        menuSelect();
     
     
     }
@@ -13,9 +18,62 @@ public class CampUI {
         System.out.println("Welcome to GoofyGobbler Summercamp!\n\n************ Main Menu ************");
         String[] menuOptions = {"Create an account", "Login", "Information on our camp", "FAQ", "Exit"};
         for(int i=0;i<menuOptions.length;i++) {
-            System.out.println((i+1)+menuOptions[i]);
+            System.out.println((i+1)+". "+menuOptions[i]);
         }
     
     }
+    
+    private static void menuSelect() {
+    	
+    	Scanner keyboard = new Scanner(System.in);
+        
+        int menuSelection;
+        
+        boolean invalidInput = false;
+        
+        do {
+        	
+        	if(invalidInput) {
+        		System.out.println("Menu option not valid, choose again...");
+        	}
+        	
+        	menuSelection = keyboard.nextInt();
+        	keyboard.nextLine();
+        	
+        	if(menuSelection == 1) {
+        		createAccountMenu();
+        	}
+        	else if(menuSelection == 2) {
+        		
+        	}
+        	else if(menuSelection == 3) {
+        		
+        	}
+        	else if(menuSelection == 4) {
+        		
+        	}
+        	else if(menuSelection == 5) {
+        		
+        	}
+        	else {
+        		invalidInput = true;
+        	}
+        	
+        	
+        } while(menuSelection > (menuLength) || menuSelection < 1);
+    
+    }
+    
+    private static void createAccountMenu() {
+    	
+    	clearScreen();
+    	System.out.println("************ Account Registration ************");
+    	
+    }
+    
+    private static void clearScreen() {
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
+	}
     
 }
