7d078189c4ceb75fc6ab2525959ca095dcf2ec2a
gward23 <grantward23@outlook.com>
2022-10-20 08:34:33 -0400

    added files

--- MedicalInfo.java
+++ MedicalInfo.java
@@ -3,23 +3,27 @@ import java.util.ArrayList;
 public class MedicalInfo {
     private ArrayList<String> allergies;
     private String guardian;
-    private String emergencyContact;
+    private Contact emergencyContact;
     private String address;
+    private ArrayList<String> conditions;
 
-    //TODO Changed emergencyContact to contact
-    //TODO add allergies to constructor
+    //TODO Changed emergencyContact to contact :)
+    //TODO add allergies to constructor :)
     //TODO add some kind of "other" medical info
     /**
      * Initializes the medical information for the user
      * 
      * @param guardian  who is in charge of the child
      * @param emergencyContact  who would the camp call if there's an emergency
-     * @param address   where thw child lives
+     * @param address   where the child lives
      */
-    public MedicalInfo(String guardian, String emergencyContact, String address) {
+    public MedicalInfo(String guardian, Contact emergencyContact, String address, ArrayList<String> allergies, ArrayList<String> conditions) {
+        this.allergies = allergies;
         this.guardian = guardian;
         this.emergencyContact = emergencyContact;
         this.address = address;
+        this.conditions = conditions;
+
     }
 
     /**
