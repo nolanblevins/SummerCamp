9ffc6af871e2aa9d0723570c93ce6810cbb953aa
gward23 <grantward23@outlook.com>
2022-10-20 08:48:27 -0400

    added stubs

--- RegisteredUser.java
+++ RegisteredUser.java
@@ -1,11 +1,48 @@
 import java.util.ArrayList;
+import java.util.UUID;
 
 public class RegisteredUser extends User{
     protected ArrayList<Child> children;
+    protected UUID id;
     
     public RegisteredUser(String firstName, String lastName, String email, String phoneNumber, String password) {
         super(firstName, lastName, email, phoneNumber, password);
+        this.id = UUID.randomUUID();
     }
+
+    public RegisteredUser(UUID id, String firstName, String lastName, String email, String phoneNumber, String password) {
+        super(firstName, lastName, email, phoneNumber, password);
+        this.id = id;
+    }
+
+    public void changeInfo(String username) {
+
+    }
+
+    public ArrayList<Activity> viewSchedule(Child child) {
+        return null;
+    }
+
+    public void registerChild(String firstName, String lastName, int age, String medicalInfo, Camp camp) {
+
+    }
+
+    public void removeChild(String firstName, String lastName) {
+
+    }
+
+    public ArrayList<String> viewChildNotes(String firstName, String lastName) {
+        return null;
+    }
+
+    public void cancelRegistration(Child child) {
+        
+    }
+
+
+
+
+
 
 
     
