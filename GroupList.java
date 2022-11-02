0f7740f9086443368efa6927161262eb2a9bf666
mmtran0210 <mi.minh.tran@gmail.com>
2022-11-02 16:38:19 -0400

    javadoc

--- GroupList.java
+++ GroupList.java
@@ -9,6 +9,11 @@ public class GroupList {
         this.groups = DataReader.loadGroups();
     }
 
+    /**
+    * Method to get an Instance of GroupList
+    *
+    * @return an Instance of groupList
+    */
     public static GroupList getInstance(){
         if(groupList == null){
             groupList = new GroupList();
@@ -16,10 +21,21 @@ public class GroupList {
         return groupList;
     }
 
+    /**
+     * Method to add a new Group 
+     *
+     * @param group 
+     */
     public void addGroup(Group group){
         this.groups.add(group);
     }
 
+    /**
+     * Method to getGroup from UUID
+     *
+     * @param uuid      Group UUID
+     * @return          group if the UUID matches, otherwise null
+     */
     public Group getGroup(UUID uuid){
         for(Group g : groups){
             if(g.getUUID().compareTo(uuid) == 0){
@@ -29,10 +45,21 @@ public class GroupList {
         return null;
     }
 
+    /**
+     * Method to get All Group in the Camp
+     *
+     * @return ArrayList of groups.
+     */
     public ArrayList<Group> getAllGroups(){
         return groups;
     }
 
+    /**
+     * Method to edit Group
+     * 
+     * @param group:
+     * 
+     */
     public void editGroup(Group group){
         for(Group g : groups){
             if(group.getUUID().compareTo(g.getUUID()) == 0){
@@ -42,6 +69,9 @@ public class GroupList {
         }
     }
 
+    /**
+     * Method to save Group from DataWriter
+     */
     public void saveGroups(){
         DataWriter.saveGroups();
     }
