96fa56544a569649e43bef30c2c8bf7def04248c
Nolan Blevins <npblevins@gmail.com>
2022-11-13 16:51:19 -0500

    testing counselor

---
+++ TestCounselor.java
@@ -0,0 +1,14 @@
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.UUID;
+import java.util.Date;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TestCounselor {
+    
+}
--- TestSchedule.java
+++ TestSchedule.java
@@ -1,3 +1,5 @@
+
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
